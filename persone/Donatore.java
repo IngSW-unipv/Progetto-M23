af5175f59f83d546b2054bd7dd7c83ea4efe3b40
DavideArioli <davide.arioli01@universitadipavia.it>
2023-01-11 12:14:31 +0100

    2

--- persone/Donatore.java
+++ persone/Donatore.java
@@ -1,6 +1,12 @@
 package persone;
 
-public class Donatore {
-public Donatore(){}
+public class Donatore extends Persona {
+    
+	
+	
+	public Donatore(String n,String c,String cf,Boolean s){
+		
+		super(n,c,cf,s);
+	}
 
 }
