6ace9d4c4b1c70a162308a4677538580063916a0
DavideArioli <davide.arioli01@universitadipavia.it>
2023-01-16 19:31:12 +0100

    risoluzione errore parte sql

--- DataBase/JavaDatabaseConn.java
+++ DataBase/JavaDatabaseConn.java
@@ -4,54 +4,70 @@ import java.sql.SQLException;
 
 
 public class JavaDatabaseConn {
-	public static Connection startConnection(Connection conn, String schema)
-	{
-	String DbDrvier=null;
-	String DbURL=null;
-	String username=null;
-	String password=null;
 	
-	DbDriver= "com.mysql.jdbc.Driver";
-	DbURL= "jdbc:mysql://localhost:3306/"+javadop;
-	username="root";
-	password ="JavaDOP123";
-	if (isOpen(conn))
-		closeConncection(conn);
-	try 
-	{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		conn = DriverManager.getConne(DbURL, username, password);
+	public static Connection startConnection(Connection conn, String schema){
 		
-	}
-	catch (Exception e) {
-		e.printStackTrace();
-		return null;
-	}
-	return conn;
+        String DbDriver=null;
+		String DbURL=null;
+		String username=null;
+		String password=null;
 	
-	}
+		DbDriver= "com.mysql.jdbc.Driver";
+		DbURL= "jdbc:mysql://localhost:3306/"+javadop;
+		username="root";
+		password ="JavaDOP123";
+		
+		
+		if (isOpen(conn)) {
+			closeConnection(conn);
+		}
+		
+		try 
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConne(DbURL, username, password);
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return conn;
+		
+		}
+	
+	
+	
+	
+		
 	public static boolean isOpen(Connection conn)
-	{
-		if (conn == null)
-			return false;
-		else return true;
-	}
-public static Connection cloceConnection(Connection conn)
-{
-	if (!isOPen(conn))
-		return null;
-	try
-	{
-		conn.close();
-		conn = null;
+		{
+			if (conn == null)
+				return false;
+			else return true;
+		}
 		
+	
+	
+	
+	
+	public static Connection closeConnection(Connection conn){
+	    	
+		if (!isOpen(conn)) {
+			return null;
+		}
+		try
+		{
+			conn.close();
+			conn = null;
+			
+		}
+		catch (SQLException e)
+		{
+		 e.printStackTrace();
+		 return null;
+		}
+		return conn;
+		}
 	}
-	catch (SQLException e)
-	{
-	 e.printStackTrace();
-	 return null;
-	}
-	return conn;
-	}
-}
-
+	
