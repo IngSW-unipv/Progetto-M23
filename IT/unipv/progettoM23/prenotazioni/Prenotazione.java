c4a544f66d4a995444edea2e9fd1fe6ec0e6de6e
Orazio Tandurella <orazio.tandurella01@universitadipavia.it>
2023-01-19 10:57:46 +0100

    ..

---
+++ IT/unipv/progettoM23/prenotazioni/Prenotazione.java
@@ -0,0 +1,17 @@
+package IT.unipv.progettoM23.prenotazioni;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import javax.xml.crypto.Data;
+import IT.unipv.progettoM23.persona.Donatore;
+
+public class Prenotazione {
+	
+	private Date data;
+
+	public Prenotazione(Donatore d, Date data) {
+		this.data =data;
+		
+	}
+
+}
